a28172c13e831d80fa91a3225bd8f1646142c15b
Nikolay <deathmetal9812@gmail.com>
2026-05-10 07:26:10 +0200

    refactor

--- components/BookEvent.tsx
+++ components/BookEvent.tsx
@@ -1,14 +1,23 @@
 "use client";
 
 import { useState } from "react";
-const BookEvent = () => {
+import { createBooking } from "@/lib/action/booking.actions";
+import posthog from "posthog-js";
+const BookEvent = ({ eventId, slug }: { eventId: string; slug: string }) => {
   const [email, setEmail] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    const { success } = await createBooking({ eventId, slug, email });
 
-    setTimeout(() => setSubmitted(true), 1000);
+    if (success) {
+      setSubmitted(true);
+      posthog.capture("event_booked", { eventId, slug, email });
+    } else {
+      console.error("Booking creation failed");
+      posthog.captureException("Booking creation failed");
+    }
   };
 
   return (
---
+++ lib/action/booking.actions.ts
@@ -0,0 +1,22 @@
+"use server";
+import connectDB from "@/lib/mongodb";
+import { Booking } from "@/database";
+
+export const createBooking = async ({
+  eventId,
+  slug,
+  email,
+}: {
+  eventId: string;
+  slug: string;
+  email: string;
+}) => {
+  try {
+    await connectDB();
+    await Booking.create({ eventId, slug, email });
+    return { success: true };
+  } catch (error) {
+    console.error("create booking failed", error);
+    return { success: false };
+  }
+};
